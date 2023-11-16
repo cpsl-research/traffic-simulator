d6f7f700897f3103028059f42541541332ead4b0
Spencer Hallyburton <20426598+roshambo919@users.noreply.github.com>
2023-11-16 10:16:27 -0500

    Add additional import to ts

--- trafficsim/__init__.py
+++ trafficsim/__init__.py
@@ -1,6 +1,7 @@
 from .core.geometry.cubic_curve import CubicCurve
 from .core.geometry.quadratic_curve import QuadraticCurve
 from .core.geometry.segment import Segment
+from .core.geometry.straight_curve import StraightCurve
 from .core.simulation import Simulation
 from .core.vehicle import Vehicle
 from .core.vehicle_generator import VehicleGenerator
